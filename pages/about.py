1dcbb71c8e6a0a1bb86ecf5131a7ab9458ab2c6c
André Luferat <contato@luferat.net>
2026-02-15 11:34:20 -0300

    Blueprints

---
+++ pages/about.py
@@ -0,0 +1,14 @@
+# pages/about.py
+
+from flask import Blueprint, render_template
+
+about_bp = Blueprint("about", __name__)
+privacy_bp = Blueprint("privacy", __name__)
+
+@about_bp.route("/about")
+def about_page():
+    return render_template("about.html")
+
+@privacy_bp.route("/privacy_bp")
+def privacy_page():
+    return render_template("privacy_bp.html")
