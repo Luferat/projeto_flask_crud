ec1e7cc70d44d5098aa5dc9c2cd046d9b7e4f01b
André Luferat <contato@luferat.net>
2026-02-19 19:38:36 -0300

    Template ok

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-# app.py
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "<h1>Olá Mundo"
+    return render_template("_base.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
