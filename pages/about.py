f03ebdaff20821e7c257a0fcf35b75b742eecfe9
André Luferat <33375030+Luferat@users.noreply.github.com>
2026-02-24 20:57:48 -0300

    Refactor about page routes in about.py

--- pages/about.py
+++ pages/about.py
@@ -1,15 +1,16 @@
+# pages/about.py
+# Páginas "estáticas" tem suas rotas aqui
+
 from flask import Blueprint, render_template
 
-info_bp = Blueprint("info", __name__)
+about_bp = Blueprint("about", __name__)
 
-
-@info_bp.route("/about")
+@about_bp.route("/about")
 def about_page():
     page_title = "Sobre..."
     return render_template("about.html", page_title=page_title)
 
-
-@info_bp.route("/privacy")
+@about_bp.route("/privacy")
 def privacy_page():
     page_title = "Políticas de Privacidade"
     return render_template("privacy.html", page_title=page_title)
